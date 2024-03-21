be20de2e794ccba233538a23ab2e3ee76546798a
rosenclever <rosenclever@gmail.com>
2024-03-21 16:01:39 -0300

    index instituicao ok

--- Academico/Controllers/InstituicaoController.cs
+++ Academico/Controllers/InstituicaoController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Academico.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Academico.Controllers
 {
     public class InstituicaoController : Controller
     {
+        private static IList<Instituicao> instituicoes = new List<Instituicao>()
+        {
+            new Instituicao()
+            {
+                InstituicaoID = 1,
+                Nome = "Hogwarts",
+                Endereco = "Escócia"
+            },
+            new Instituicao()
+            {
+                InstituicaoID = 2,
+                Nome = "Mansão X",
+                Endereco = "New York"
+            }
+        };
         public IActionResult Index()
         {
-            return View();
+            return View(instituicoes);
         }
     }
 }
